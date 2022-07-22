d004caa476c90da6f25652ae8aedbb5e0b795a76
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-22 17:07:12 +0100

    email sending to users, missing templates

--- src/api/job/controllers/job.js
+++ src/api/job/controllers/job.js
@@ -1,9 +1,37 @@
 'use strict';
 
-/**
- *  job controller
- */
+
+/* job controller */
+
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::job.job');
+
+
+/* dublinhospitalityjobs@protonmail.com */
+
+
+
+
+/* module.exports = {
+
+    send: async (ctx) => {
+        let options = ctx.request.body;
+
+        try {
+            //Send email to the user
+            await strapi.plugins['email'].services.email.send({
+                to: options.to,
+                from: 'dublinhospitalityjobs@protonmail.com',
+                subject: options.subject,
+                html: options.html
+            });
+            ctx.send({ ok: true })
+        } catch (err) {
+            return ctx.badRequest(null, err);
+
+        }
+
+    }
+} */
