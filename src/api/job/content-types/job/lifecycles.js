60ed13732782c25d3c1806687489d9ac7b33dfba
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-30 20:07:50 +0100

    chore: imprving text

--- src/api/job/content-types/job/lifecycles.js
+++ src/api/job/content-types/job/lifecycles.js
@@ -5,10 +5,10 @@ module.exports = {
         try {
             await strapi.plugins['email'].services.email.send({
                 to: `${result.email}`,
-                bcc: "dublinhospitalityjobs@protonmail.com",
                 from: "dublinhospitalityjobs@protonmail.com",
+                bcc: "dublinhospitalityjobs@protonmail.com",
                 subject: "Dublin Hospitality Jobs -We have received your message/job position",
-                text: `<h1> Thanks ${result.email}! We have received your job </h1>
+                html: `<h1> Thanks ${result.email}! We have received your job </h1>
                 <p> it will be reviewed and posted within 24 hs if not issue was found. </p>
                 <hr> </hr>
                 <br> </br>
