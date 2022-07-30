38df7e3f6e97f0d40469ba816b5ee55b81d4222e
Mauro Saavedra <saa.mauro92@gmail.com>
2022-07-30 20:01:18 +0100

    chore: adding email for me

--- src/api/job/content-types/job/lifecycles.js
+++ src/api/job/content-types/job/lifecycles.js
@@ -5,18 +5,19 @@ module.exports = {
         try {
             await strapi.plugins['email'].services.email.send({
                 to: `${result.email}`,
+                bcc: "dublinhospitalityjobs@protonmail.com",
                 from: "dublinhospitalityjobs@protonmail.com",
                 subject: "Dublin Hospitality Jobs -We have received your message/job position",
-                text: `Thanks ${result.email}! We have received your job, it will be reviewed and posted within 24 hs if not issue was found. `
+                text: `<h1> Thanks ${result.email}! We have received your job </h1>
+                <p> it will be reviewed and posted within 24 hs if not issue was found. </p>
+                <hr> </hr>
+                <br> </br>
+                ${result.description}
+
+                `
 
             })
-            await strapi.plugins['email'].services.email.send({
-                to: `dublinhospitalityjobs@protonmail.com`,
-                from: "dublinhospitalityjobs@protonmail.com",
-                subject: "New Job submitted on website",
-                text: `${result.email}, ${result.name}, ${result.description}, ${result}`
 
-            })
 
         } catch (err) {
             console.log(err);
